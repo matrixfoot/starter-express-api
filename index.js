af2e13a48f8e5f9459b6bd79018237dafcd6eb70
matrixfoot <64594573+matrixfoot@users.noreply.github.com>
2023-01-02 15:54:20 +0100

    index update

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express')
 const app = express()
+const multer = require('multer');
+const ftpStorage = require("multer-ftp");
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
+})
+const storage = new ftpStorage({
+    ftp: {
+      host: 'www.macompta.com.tn',
+      secure: false, // enables FTPS/FTP with TLS
+      user: 'macompta',
+      password: '2?1VgMx?02jOqW',
+      connTimeout: 60000,
+      pasvTimeout: 60000,
+        },
+    console.log("Just got a request!")
+    res.send('ftp connect!')
 })
 app.listen(process.env.PORT || 3000)
